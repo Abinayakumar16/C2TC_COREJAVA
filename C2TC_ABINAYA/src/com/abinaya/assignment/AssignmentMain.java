54621d686afcbe49a451cadc793d4028158a8e45
Abi <Abi@DESKTOP-F6ILIM2>
2024-12-16 10:43:41 +0530

    Assignment_corejava

---
+++ C2TC_ABINAYA/src/com/abinaya/assignment/AssignmentMain.java
@@ -0,0 +1,12 @@
+package com.abinaya.assignment;
+import com.abinaya.assignment.employees.Manager;
+import com.abinaya.assignment.employees.Developer;
+import com.abinaya.assignment.utilities.EmployeeUtilities;
+public class AssignmentMain {
+ public static void main(String[] args) {
+ Manager manager = new Manager("John", 101, 90000, "Sales");
+ Developer developer = new Developer("Alice", 102, 80000, "Java");
+ EmployeeUtilities.printEmployeeDetails(manager);
+ EmployeeUtilities.printEmployeeDetails(developer);
+ }
+}
